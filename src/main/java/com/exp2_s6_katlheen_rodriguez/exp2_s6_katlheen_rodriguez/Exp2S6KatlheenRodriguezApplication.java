1315d9728e65e80158d5ce34bba382887c7df11b
Katlheen Rodriguez <kv.rodriguezgarcia@gmail.com>
2025-04-18 19:24:55 -0400

    Proyecto SpringBoot

---
+++ src/main/java/com/exp2_s6_katlheen_rodriguez/exp2_s6_katlheen_rodriguez/Exp2S6KatlheenRodriguezApplication.java
@@ -0,0 +1,13 @@
+package com.exp2_s6_katlheen_rodriguez.exp2_s6_katlheen_rodriguez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp2S6KatlheenRodriguezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp2S6KatlheenRodriguezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exp2_s6_katlheen_rodriguez/exp2_s6_katlheen_rodriguez/Exp2S6KatlheenRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp2_s6_katlheen_rodriguez.exp2_s6_katlheen_rodriguez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp2S6KatlheenRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
